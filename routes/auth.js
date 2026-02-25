9bfbc7c32ff52acbd0bb20b8fe7c03b9594c80d2
Luka Catela <luca.catela8@gmail.com>
2026-02-25 15:39:07 +0100

    Dodane rute register i login

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import connectToDatabase from "../config/db";
+import connectToDatabase from "../config/db.js";
 
 const router = Router();
-
-const db = await connectToDatabase();
 
 // POST /api/auth/register
 router.post("/register", async (req, res) => {
   try {
+    const db = await connectToDatabase();
+
     const { name, surname, email, password, role } = req.body;
 
     if (!name || !surname || !email || !password)
@@ -38,3 +38,35 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
+// POST /api/auth/login
+router.post("/login", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ error: "Potrebna sva polja" });
+
+    const user = await db.collection("users").findOne({ email });
+    if (!user) return res.status(400).json({ error: "Krivi podaci" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Krivi podaci" });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+
+    res.json({
+      token,
+      user: { id: user._id, name: user.name, role: user.role },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+export default router;
