8204435e0ce436cc47ac19aeec559ffd2ffcf4db
Luka Catela <luca.catela8@gmail.com>
2026-03-02 12:33:56 +0100

    ispravljena greskica na bazi

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 import { MongoClient } from "mongodb";
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 const cluster = process.env.MONGO_CLUSTER;
+const baza = process.env.MONGO_DB_NAME;
 
 const mongo_URI = `mongodb+srv://${username}:${password}@${cluster}.dxkfyc9.mongodb.net/?appName=${cluster}`;
 
@@ -14,16 +15,16 @@ const mongo_URI = `mongodb+srv://${username}:${password}@${cluster}.dxkfyc9.mong
 let db;
 
 async function connectToDatabase() {
-    if (db) return db;
-    try {
-        const client = new MongoClient(mongo_URI);
-        await client.connect();
-        console.log("Baza uspjesno povezana");
-        db = client.db("");
-        return db
-    } catch (error) {
-        console.error("Connection error: ", error);
-    }
+  if (db) return db;
+  try {
+    const client = new MongoClient(mongo_URI);
+    await client.connect();
+    console.log("Baza uspjesno povezana");
+    db = client.db(baza);
+    return db;
+  } catch (error) {
+    console.error("Connection error: ", error);
+  }
 }
 
 export default connectToDatabase;
