631550ff1c90ce590ed44c0a1c37067c68d7e626
Luka Catela <luca.catela8@gmail.com>
2026-03-03 14:18:00 +0100

    finisirana ruta auth

--- routes/auth.js
+++ routes/auth.js
@@ -78,7 +78,7 @@ router.get("/me", authMiddleware, async (req, res) => {
     const user = await db
       .collection("users")
       .findOne(
-        { _id: new ObjectId(req.body.id) },
+        { _id: new ObjectId(req.user.id) },
         { projection: { password: 0 } },
       );
     res.status(200).json(user);
