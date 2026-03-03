a33a09662c966718c61fffc8b86d231736c33415
Luka Catela <luca.catela8@gmail.com>
2026-03-03 15:10:21 +0100

    updejtane rute

--- routes/profiles.js
+++ routes/profiles.js
@@ -31,7 +31,7 @@ router.post("/", authMiddleware, async (req, res) => {
     const newProfile = {
       user_id: new ObjectId(req.user.id),
       bio: req.body.bio || "",
-      study_year: req.body.department || "",
+      study_year: req.body.study_year || "",
       department: req.body.department || "",
       skill: req.body.skill || [],
       social: {
@@ -50,10 +50,47 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
-//PUT    /:userId  -- updejtaj moj profil (user)
-router.put("/:userID", async (req, res) => {});
+//PATCH   /:userId  -- updejtaj cijeli moj profil (user)
+router.put("/:userId", authMiddleware, async (req, res) => {
+  if (req.user.id !== req.params.userId)
+    return res.status(403).json({ error: "Pristup zabranjen" });
+
+  try {
+    const db = await connectToDatabase();
+    await db.collection("profiles").updateOne(
+      { user_id: new ObjectId(req.params.userId) },
+      {
+        $set: {
+          bio: req.body.bio,
+          study_year: req.body.study_year,
+          department: req.body.department,
+          skill: req.body.skill,
+          social: req.body.social,
+          updatedAt: new Date(),
+        },
+      },
+    );
+
+    res.status(200).json({ message: "Profil updejtan" });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 //DELETE /:userId  -- izbrisi profil (admin ili user)
-router.delete("/:userID", async (req, res) => {});
+router.delete("/:userId", authMiddleware, async (req, res) => {
+  if (req.user.role !== "admin" && req.user.id !== req.params.userId)
+    return res.status(403).json({ error: "Pristup zabranjen" });
+
+  try {
+    const db = await connectToDatabase();
+    await db
+      .collection("profiles")
+      .deleteOne({ user_id: new ObjectId(req.params.userId) });
+    res.status(200).json({ message: "Profil izbrisan" });
+  } catch (error) {
+    res.status(500).json({ error: "Serrver error" });
+  }
+});
 
 export default router;
