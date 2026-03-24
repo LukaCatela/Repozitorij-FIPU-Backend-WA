135deae419bb8122f7297615b264079397e69c13
Luka Catela <luca.catela8@gmail.com>
2026-03-24 16:00:36 +0100

    dodane libraries
    Libraries za sigurnost

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import express from "express";
 import cors from "cors";
 import connectToDatabase from "./config/db.js";
+import helmet from "helmet";
+import morgan from "morgan";
 
 import authRouter from "./routes/auth.js";
 import profilesRouter from "./routes/profiles.js";
 import usersRouter from "./routes/users.js";
-//import projektiRouter from './routes/projects.js';
+import projektiRouter from "./routes/projects.js";
 //const express = require('express'); //jer smo stavili u package.json type:module umisto commonjs
-const router = express.Router();
 
 const app = express();
 
 const PORT = 3000;
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
+app.use(helmet({ crossOriginResourcePolicy: false })); //secures HTTP headers, protects against common attacks -> pojasnjenje
+app.use(morgan("dev")); // za laski ispis log ruta
 
 connectToDatabase().then(() => {
   app.use("/users", usersRouter);
-  //app.use("/api/projects", projektiRouter);
+  app.use("/projects", projektiRouter);
   app.use("/auth", authRouter);
   app.use("/profiles", profilesRouter);
 
@@ -31,4 +34,3 @@ connectToDatabase().then(() => {
     }
   });
 });
-export default router;
