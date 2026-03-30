4a9f96923e9302f7ce6bd38a3550907b02b307b7
Luka Catela <luca.catela8@gmail.com>
2026-03-30 15:09:20 +0200

    dodana ruta media za prihvat dokumenata

---
+++ routes/media.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import connectToDatabase from "../config/db.js";
+import { ObjectId } from "mongodb";
+
+const router = Router();
+
+router.get("/:projectId", async (req, res) => {
+  const db = await connectToDatabase();
+  const files = await db
+    .collection("media")
+    .find({ projectId: new ObjectId(req.params.projectId) })
+    .toArray();
+  res.json(files);
+});
+
+export default router;
